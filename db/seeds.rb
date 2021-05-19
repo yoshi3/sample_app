5f6acdf7b494b7b66a055d14b26abafd74344210
yoshi3 <kokono1220@gmail.com>
2021-05-19 19:16:33 +0900

    :apple:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
+# メインのサンプルユーザーを1人作成する
 User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin: true)
+  admin:     true,
+  activated: true,
+  activated_at: Time.zone.now)
 
+# 追加のユーザーをまとめて生成する
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -11,5 +15,7 @@ User.create!(name:  "Example User",
   User.create(name: name,
     email: email,
     password: password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
 end
